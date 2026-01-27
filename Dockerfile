e97061fd7adda550012c1c8af63881eb4382b20a
yakhlafhoussam <yakhlafhoussam@gmail.com>
2026-01-27 10:52:52 +0100

    Add structure de project whit laravel and docker and create the database

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Base image
-FROM php:8.3-fpm-alpine
+FROM php:8.4-fpm-alpine
 
 RUN apk add --no-cache \
     bash \
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
